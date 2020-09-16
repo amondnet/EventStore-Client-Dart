532a66d22f5180f5f88359a78f462d7645fb9918
Minsu Lee <amond@amond.net>
2020-09-17 02:17:06 +0900

    testing: append test

--- tool/run_test_container.sh
+++ tool/run_test_container.sh
@@ -1,2 +1,2 @@
-docker run -p 1113:1113 -p 2113:2113 -e EVENTSTORE_DEV=true \
+docker run -p 1113:1113 -p 2113:2113 -e EVENTSTORE_DEV=true -e EVENTSTORE_ENABLE_EXTERNAL_TCP=true \
   docker.pkg.github.com/eventstore/eventstore-client-grpc-testdata/eventstore-client-grpc-testdata:20.6.0-buster-slim
